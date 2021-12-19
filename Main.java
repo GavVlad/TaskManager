9a694f0339dd15676e5e2510f20838b2c240a778
iamevvgen <96377813+iamevvgen@users.noreply.github.com>
2021-12-19 19:37:37 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        task one = new task("NAME", "DESC", 2022, 11, 23, 12, 34, 00, "admin@admin.ru" );
+        task one = new task("NAME", "DESC", 2027, 11, 23, 12, 34, 00, "admin@admin.ru" );
+        System.out.println(one.getDate());
+        one.setDate(2023, 11, 23, 12, 34, 00);
         System.out.println(one.getDate());
     }
 }
